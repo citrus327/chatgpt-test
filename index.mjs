aa1218b790b75ff1c22e866d3e6d16dd9cd242a4
citrus327 <citrus327@outlook.com>
2023-02-16 10:40:12 +0800

    feat: remove comments

--- index.mjs
+++ index.mjs
@@ -19,11 +19,3 @@ const run = async () => {
 };
 
 run()
-// const Koa = require("koa");
-// const app = new Koa();
-
-// app.use(async (ctx) => {
-//   ctx.body = "Hello World";
-// });
-
-// app.listen(3000);
